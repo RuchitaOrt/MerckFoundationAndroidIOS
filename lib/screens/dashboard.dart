f26a2ff79cf2ef2371d10156351f73093b94dcc9
Archana527 <archana.onerooftech@gmail.com>
2020-12-30 19:13:03 +0530

    commiting changes

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -60,7 +60,7 @@ class _DashboardState extends State<Dashboard>
             Ourprogram(),
             Stories(),
             NewsPage(),
-            CallforApplication()
+            //CallforApplication()
           ]),
       bottomNavigationBar: BottomNavyBar(
         selectedIndex: currentIndex,
