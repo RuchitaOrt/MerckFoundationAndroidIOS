a8e58eaf1e98c7c08a6e406b74cf8da7cd1ba96c
ruchita <ruchitaj.onerooftech@gmail.com>
2021-03-31 17:49:44 +0530

    commit

--- lib/screens/ourPrograms/Merckxanceraccess.dart
+++ lib/screens/ourPrograms/Merckxanceraccess.dart
@@ -1211,13 +1211,15 @@ class MerckCanceraccessState extends State<MerckCanceraccess>
                 itemBuilder: (BuildContext context, int index) {
                   return GestureDetector(
                     onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (BuildContext context) => VideoPlayer(
-                                    videoUrl: GlobalLists
-                                        .mmtmmedialist[index].mediaUrl,
-                                  )));
+                      // Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute(
+                      //         builder: (BuildContext context) => VideoPlayer(
+                      //               videoUrl: GlobalLists
+                      //                   .mmtmmedialist[index].mediaUrl,
+                      //             )));
+                      ShowDialogs.launchURL(
+                          GlobalLists.mmtmmedialist[index].mediaUrl);
                     },
                     child: Padding(
                       padding: const EdgeInsets.only(right: 8, left: 10),
