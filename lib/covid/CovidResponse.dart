fc8faecb3986f68eb621f278279ccd4ed2db86f2
Archana527 <archana.onerooftech@gmail.com>
2021-04-05 22:55:05 +0530

    commiting chnages

--- lib/covid/CovidResponse.dart
+++ lib/covid/CovidResponse.dart
@@ -62,17 +62,17 @@ class CovidResponseState extends State<CovidResponse>
   List<Newsletter> newslist = [
     Newsletter(
         imge:
-            "http://merckfoundation.org/mfindia/web/public/img/covid-news1.png",
+            "http://merck-foundation.com/merckfoundation/public/uploads/merck_media/1614855408_096ac4d715d3daaea5a6.jpg",
         title:
             "Merck Foundation announce ‘Stay at Home’ Media Recognition Awards…"),
     Newsletter(
         imge:
-            "http://merckfoundation.org/mfindia/web/public/img/covid-news2.png",
+            "https://smitlalai.com/demo/merck/public/assets/plugins/ckfinder/core/connector/php/connector.php?command=Proxy&lang=en&type=Files&currentFolder=%2F&hash=36d151d14dfb6a22&fileName=KNS_3249.jpg",
         title:
             "Message from Dr. Rasha Kelej on Community donations provided in…"),
     Newsletter(
         imge:
-            "http://merckfoundation.org/mfindia/web/public/img/covid-news3.png",
+            "http://merck-foundation.com/merckfoundation/public/uploads/newsletter/1613074147_50c0511266c18acc1b79.jpg",
         title:
             "Merck Foundation announce ‘Stay at Home’ Media Recognition Awards for…")
   ];
