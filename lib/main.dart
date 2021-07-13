57afc1af50ace68444826517e6e395cf314dd9be
ruchita <ruchitaj.onerooftech@gmail.com>
2021-07-13 21:19:10 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:merckfoundation22dec/screens/dashboard.dart';
 import 'package:merckfoundation22dec/screens/splash.dart';
 import 'package:merckfoundation22dec/utility/GlobalLists.dart';
 import 'package:merckfoundation22dec/utility/UtilityFile.dart';
@@ -94,31 +96,118 @@ class _MyAppState extends State<MyApp> {
 
     FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
       print("onlisten");
-      print(message.data);
-      print(message.notification.body);
-      print(message.notification.title);
-      RemoteNotification notification = message.notification;
-      print(notification);
 
-      var imgurl = message.notification.android.imageUrl;
+      var title = message.notification.title;
+      var body = message.notification.body;
+      String imgurl = message.notification.android.imageUrl;
       print("imgurl");
       print(imgurl);
+      //   final String largeIconPath = await _downloadAndSaveFile(
+      //     'https://via.placeholder.com/48x48', 'largeIcon');
+      final String bigPicturePath =
+          await _downloadAndSaveFile(imgurl, 'bigPicture');
+      final BigPictureStyleInformation bigPictureStyleInformation =
+          BigPictureStyleInformation(FilePathAndroidBitmap(bigPicturePath),
+              largeIcon: FilePathAndroidBitmap(bigPicturePath),
+              contentTitle: title.toString(),
+              htmlFormatContentTitle: true,
+              summaryText: body,
+              htmlFormatSummaryText: true);
+      final AndroidNotificationDetails androidPlatformChannelSpecifics =
+          AndroidNotificationDetails('big text channel id',
+              'big text channel name', 'big text channel description',
+              styleInformation: bigPictureStyleInformation);
+      final NotificationDetails platformChannelSpecifics = NotificationDetails(
+        android: AndroidNotificationDetails(
+            channel.id, channel.name, channel.description,
+            // TODO add a proper drawable resource to android, for now using
+            //      one that already exists in example app.
+            icon: '@mipmap/ic_launcher',
+            styleInformation: bigPictureStyleInformation),
+      );
+      await flutterLocalNotificationsPlugin.show(
+        0, title.toString(),
+        body.toString(), platformChannelSpecifics,
+        //  payload: jsonEncode(message)
+      );
+
+      var data = message.data['FLUTTER_NOTIFICATION_CLICK'];
+      print(data);
+      // Data data = Data(
+      //   clickAction: msg['click_action'],
+      //   sound: msg['sound'],
+      //   status: msg['status'],
+      //   screen: msg['screen'],
+      //   extradata: msg['extradata'],
+      // );
+      switch (data.toString()) {
+        case "datascreen":
+          // Navigator.push(
+          //     context,
+          //     MaterialPageRoute(
+          //         builder: (BuildContext context) => Dashboard(
+          //               index: 2,
+          //             )));
+          // navigatorKey.currentState
+          //     .push(MaterialPageRoute(builder: (_) => Dashboard(index: 2)));
+          // Navigator.push(navigatorKey.currentState.context,
+          //     MaterialPageRoute(builder: (context) => Dashboard(index: 2)));
+          // SchedulerBinding.instance.addPostFrameCallback((_) {
+          //   Navigator.push(navigatorKey.currentState.context,
+          //       MaterialPageRoute(builder: (context) => Dashboard(index: 2)));
+          //   // Navigator.of(GlobalVariable.navState.currentContext)
+          //   //     .push(MaterialPageRoute(
+          //   //         builder: (context) => TimelineView(
+          //   //               campaignId: message["data"]["campaign"],
+          //   //             ))
+          //   // );
+          // });
+          break;
+        default:
+          break;
+      }
+      // if (message != null) {
+      //   // Navigator.push(
+      //   //     context,
+      //   //     MaterialPageRoute(
+      //   //         builder: (BuildContext context) => Dashboard(
+      //   //               index: 2,
+      //   //             )));
+      //   navigatorKey.currentState
+      //       .push(MaterialPageRoute(builder: (_) => Dashboard(index: 2)));
+      // }
+// String jsonEncoder() {
+//     Map testDataMap =message.notification;
+//     String jsonStringData = jsonEncode(testDataMap);
+//     return jsonStringData;
+// }
+      // flutterLocalNotificationsPlugin.show(
+//         //     notification.hashCode,
+//         //     notification.title,
+//         //     notification.body,
+//         //     NotificationDetails(
+//         //       android: AndroidNotificationDetails(
+//         //         channel.id,
+//         //         channel.name,
+//         //         channel.description,
+//         //         // TODO add a proper drawable resource to android, for now using
+//         //         //      one that already exists in example app.
+//         //         icon: '@mipmap/ic_launcher',
+//         //       ),
+//         //     ));
+
 //
 //  final String largeIconPath = await _downloadAndSaveFile(
 //         'https://via.placeholder.com/48x48', 'largeIcon');
-      final String bigPicturePath =
-          await _downloadAndSaveFile(imgurl, 'bigPicture');
-      print(bigPicturePath);
-      final BigPictureStyleInformation bigPictureStyleInformation =
-          BigPictureStyleInformation(
-        FilePathAndroidBitmap(bigPicturePath),
-        largeIcon: FilePathAndroidBitmap(bigPicturePath),
-        // contentTitle: 'Merck Foundation',
-        // htmlFormatContentTitle: true,
-        // summaryText: 'summary <i>text</i>',
-        // htmlFormatSummaryText: true
-      );
-      print(bigPictureStyleInformation.bigPicture);
+      // final String bigPicturePath =
+      //     await _downloadAndSaveFile(imgurl, 'bigPicture');
+      // final BigPictureStyleInformation bigPictureStyleInformation =
+      //     BigPictureStyleInformation(FilePathAndroidBitmap(bigPicturePath),
+      //         largeIcon: FilePathAndroidBitmap(bigPicturePath),
+      //         contentTitle: 'Merck Foundation',
+      //         htmlFormatContentTitle: true,
+      //         summaryText: 'summary <i>text</i>',
+      //         htmlFormatSummaryText: true);
       // var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
       //     'default_notification_channel_id',
       //     'Notification Channel',
@@ -128,44 +217,96 @@ class _MyAppState extends State<MyApp> {
       //     importance: Importance.max,
       //     priority: Priority.high,
       //     styleInformation: bigPictureStyleInformation);
-      final AndroidNotificationDetails androidPlatformChannelSpecifics =
-          AndroidNotificationDetails('big text channel id',
-              'big text channel name', 'big text channel description',
-              styleInformation: bigPictureStyleInformation);
-      final NotificationDetails platformChannelSpecifics =
-          NotificationDetails(android: androidPlatformChannelSpecifics);
+      // final AndroidNotificationDetails androidPlatformChannelSpecifics =
+      //     AndroidNotificationDetails('big text channel id',
+      //         'big text channel name', 'big text channel description',
+      //         styleInformation: bigPictureStyleInformation);
+      // final NotificationDetails platformChannelSpecifics =
+      //     NotificationDetails(android: androidPlatformChannelSpecifics);
       // var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
       // var platformChannelSpecifics = NotificationDetails(
       //     android: androidPlatformChannelSpecifics,
       //     iOS: iOSPlatformChannelSpecifics);
 //
 
-      print("showing");
-       flutterLocalNotificationsPlugin.show(
-          0, notification.title, notification.body, platformChannelSpecifics);
-      //  payload: jsonEncode(message));
-      print("show");
-      AndroidNotification android = message.notification?.android;
-      if (notification != null && android != null) {
-        // flutterLocalNotificationsPlugin.show(
-        //     0, notification.title, notification.body, platformChannelSpecifics,
-        //     payload: jsonEncode(message));
-        // print("show");
-        // flutterLocalNotificationsPlugin.show(
-        //     notification.hashCode,
-        //     notification.title,
-        //     notification.body,
-        //     NotificationDetails(
-        //       android: AndroidNotificationDetails(
-        //         channel.id,
-        //         channel.name,
-        //         channel.description,
-        //         // TODO add a proper drawable resource to android, for now using
-        //         //      one that already exists in example app.
-        //         icon: '@mipmap/ic_launcher',
-        //       ),
-        //     ));
-      }
+      // flutterLocalNotificationsPlugin.show(
+      //   0, title.toString(), body.toString(), platformChannelSpecifics,
+      //   payload: message
+      // payload: jsonEncode(message)
+      //  );
+      //  showNotification(message);
+//       print(message.data);
+//       print(message.notification.body);
+//       print(message.notification.title);
+//       RemoteNotification notification = message.notification;
+//       print(notification);
+
+//       var imgurl = message.notification.android.imageUrl;
+//       print("imgurl");
+//       print(imgurl);
+// //
+// //  final String largeIconPath = await _downloadAndSaveFile(
+// //         'https://via.placeholder.com/48x48', 'largeIcon');
+//       final String bigPicturePath =
+//           await _downloadAndSaveFile(imgurl, 'bigPicture');
+//       print(bigPicturePath);
+//       final BigPictureStyleInformation bigPictureStyleInformation =
+//           BigPictureStyleInformation(
+//         FilePathAndroidBitmap(bigPicturePath),
+//         largeIcon: FilePathAndroidBitmap(bigPicturePath),
+//         // contentTitle: 'Merck Foundation',
+//         // htmlFormatContentTitle: true,
+//         // summaryText: 'summary <i>text</i>',
+//         // htmlFormatSummaryText: true
+//       );
+//       print(bigPictureStyleInformation.bigPicture);
+//       // var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+//       //     'default_notification_channel_id',
+//       //     'Notification Channel',
+//       //     'MerckFoundation',
+//       //     playSound: true,
+//       //     enableVibration: true,
+//       //     importance: Importance.max,
+//       //     priority: Priority.high,
+//       //     styleInformation: bigPictureStyleInformation);
+//       final AndroidNotificationDetails androidPlatformChannelSpecifics =
+//           AndroidNotificationDetails('big text channel id',
+//               'big text channel name', 'big text channel description',
+//               styleInformation: bigPictureStyleInformation);
+//       final NotificationDetails platformChannelSpecifics =
+//           NotificationDetails(android: androidPlatformChannelSpecifics);
+//       // var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
+//       // var platformChannelSpecifics = NotificationDetails(
+//       //     android: androidPlatformChannelSpecifics,
+//       //     iOS: iOSPlatformChannelSpecifics);
+// //
+
+//       print("showing");
+//        flutterLocalNotificationsPlugin.show(
+//           0, notification.title, notification.body, platformChannelSpecifics);
+//       //  payload: jsonEncode(message));
+//       print("show");
+//       AndroidNotification android = message.notification?.android;
+//       if (notification != null && android != null) {
+//         // flutterLocalNotificationsPlugin.show(
+//         //     0, notification.title, notification.body, platformChannelSpecifics,
+//         //     payload: jsonEncode(message));
+//         // print("show");
+//         // flutterLocalNotificationsPlugin.show(
+//         //     notification.hashCode,
+//         //     notification.title,
+//         //     notification.body,
+//         //     NotificationDetails(
+//         //       android: AndroidNotificationDetails(
+//         //         channel.id,
+//         //         channel.name,
+//         //         channel.description,
+//         //         // TODO add a proper drawable resource to android, for now using
+//         //         //      one that already exists in example app.
+//         //         icon: '@mipmap/ic_launcher',
+//         //       ),
+//         //     ));
+//       }
     });
 
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
@@ -175,17 +316,20 @@ class _MyAppState extends State<MyApp> {
     super.initState();
   }
 
+  final GlobalKey<NavigatorState> navigatorKey =
+      GlobalKey(debugLabel: "Main Navigator");
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Merck Foundation',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: SplashScreen(),
-    );
+        title: 'Merck Foundation',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: SplashScreen(),
+        navigatorKey: navigatorKey);
   }
 
   settoken() async {
